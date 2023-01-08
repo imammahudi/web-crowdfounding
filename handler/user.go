7ac378c1f13fd8bc1619034333c76b21b66a28ac
imammahudi <imammahudi233@gmail.com>
2023-01-08 16:48:57 +0700

    Loggin User Endpoint

--- handler/user.go
+++ handler/user.go
@@ -16,9 +16,9 @@ func NewUserHandler(userService user.Service) *userHandler {
 	return &userHandler{userService}
 }
 
-func (h *userHandler) RegisterUser(c *gin.Context){
+func (h *userHandler) RegisterUser(c *gin.Context) {
 	var input user.RegisterUserInput
-	
+
 	err := c.ShouldBindJSON(&input)
 	if err != nil {
 		errors := helper.FormatValidationError(err)
@@ -28,16 +28,49 @@ func (h *userHandler) RegisterUser(c *gin.Context){
 		return
 	}
 
-	user, err := h.userService.RegisterUser(input)
+	newUser, err := h.userService.RegisterUser(input)
 	if err != nil {
 		response := helper.APIResponse("Register Failed", http.StatusBadRequest, "error", nil)
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
 
-	response := helper.APIResponse("Account has been resgistered", http.StatusOK, "success", user)
+	formatter := user.FormateUser(newUser, "Tokentokentoken")
+
+	response := helper.APIResponse("Account has been resgistered", http.StatusOK, "success", formatter)
 	c.JSON(http.StatusOK, response)
 
+}
 
-	
+func (h *userHandler) Login(c *gin.Context){
+	// user memasukkan email dan password
+	// input ditangkap handler
+	// mapping dari input user ke input struct
+	// di service mencari dg bantuan repository user dengan email x
+	// mencocokkan password
+
+	var input user.LoginInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+		response := helper.APIResponse("Login Failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	loggedinUser, err := h.userService.Login(input)
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+
+		response := helper.APIResponse("Login Failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	formatter := user.FormateUser(loggedinUser, "tokentokentoken")
+
+	response := helper.APIResponse("Successfully Login", http.StatusOK, "success", formatter)
+	c.JSON(http.StatusOK, response)
 }
--- main.go
+++ main.go
@@ -22,25 +22,13 @@ func main() {
 	userService := user.NewService(userRepository)
 	
 	userHandler := handler.NewUserHandler(userService)
+	
 	router := gin.Default()
 	api := router.Group("/api/v1")
 
 	api.POST("/users", userHandler.RegisterUser)
+	api.POST("/sessions", userHandler.Login)
 
 	router.Run()
 }
 
-// func handler(c *gin.Context){
-// 	dsn := "root:@tcp(127.0.0.1:3306)/crowdfounding?charset=utf8mb4&parseTime=True&loc=Local"
-// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-// 	if err != nil {
-// 		log.Fatal(err.Error())
-// 	}
-
-// 	var users []user.User
-// 	db.Find(&users)
-
-// 	c.JSON(http.StatusOK, users)
-
-// }
